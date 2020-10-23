9c0fa66e43f245cd4097d031a14caf98e9612780
askar200018 <imangali950@gmail.com>
2020-10-23 19:24:45 +0600

    added

--- mid-project/src/pages/Profile/Profile.tsx
+++ mid-project/src/pages/Profile/Profile.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useActiveUser } from '../../contexts/ActiveUserContext';
+import './Profile.scss';
 
 interface Props {}
 
 const Profile = (props: Props) => {
   const { activeUser, setActiveUser } = useActiveUser()!;
   return (
-    <div>
-      <h1>{activeUser.email}</h1>
-      <h1>{activeUser.name}</h1>
-      <h1>{activeUser.password}</h1>
-      <h1>{activeUser.profession}</h1>
+    <div className="wrapper-profile">
+      <h1 className="active-user">{activeUser.email}</h1>
+      <h1 className="active-user">{activeUser.name}</h1>
+      <h1 className="active-user">{activeUser.password}</h1>
+      <h1 className="active-user">{activeUser.profession}</h1>
     </div>
   );
 };
