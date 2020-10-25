381f7e58ad04a2a7e3f47e9bb2fc3bbb5fa9bdf0
Nuraiym-Netullina <nurishok_2000@mail.ru>
2020-10-25 15:27:49 +0600

    comment

--- mid-project/src/pages/home/index.tsx
+++ mid-project/src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './home.module.css';
 import { Link } from 'react-router-dom';
-
+import {Main} from '../main/Main'
 
 export const Home: React.FC = () => {
   return (
@@ -16,6 +16,7 @@ export const Home: React.FC = () => {
           </div> */}
         </div>
       </div>
+      <Main />
     </main>
   );
 };
